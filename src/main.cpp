2da282cdf95f0c4b5e86eaa7f02efa7d59cfbf86
C4e10VeK <tank.t3445@gmail.com>
2021-11-13 11:26:38 +1000

    refact

--- src/main.cpp
+++ src/main.cpp
@@ -30,76 +30,6 @@ bool searchMonoModule(const HandleView &view, HModule &monoModule)
 	return false;
 }
 
-extern "C" __declspec(dllexport) MonoAssembly *inject(const char *dllPath, const char *nmspaceName, const char *className, const char *methodName)
-{
-	if constexpr (sizeof(void*) != 8)
-	{
-		printf("x32 system doesnt support!\n");
-		return nullptr; 
-	}
-
-	HANDLE hProcess;
-	HModule monoModule;
-	HWND hwnd = FindWindowA(nullptr, "Risk of Rain 2");
-
-	DWORD procId = 0; 
-	GetWindowThreadProcessId(hwnd, &procId);
-
-	hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, procId);
-
-	if (!hProcess)
-	{
-		printf("Not found hProcess\n");
-		return nullptr;
-	}
-
-	HandleView hv(hProcess);
-
-	if (!searchMonoModule(hv, monoModule)) return nullptr;
-
-	Injector inj(&hv, monoModule);
-	MonoAssembly *assm = inj.inject(
-		dllPath,
-		nmspaceName,
-		className,
-		methodName	
-	);
-
-	return assm;
-}
-
-extern "C" __declspec(dllexport) void enject(MonoAssembly *assembly, const char *nmspaceName, const char *className, const char *methodName)
-{
-	if constexpr (sizeof(void*) != 8)
-	{
-		printf("x32 system doesnt support!\n");
-		return; 
-	}
-
-	HANDLE hProcess;
-	HModule monoModule;
-	HWND hwnd = FindWindowA(nullptr, "Risk of Rain 2");
-
-	DWORD procId = 0; 
-	GetWindowThreadProcessId(hwnd, &procId);
-
-	hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, procId);
-
-	if (!hProcess)
-	{
-		printf("Not found hProcess\n");
-		return;
-	}
-
-	HandleView hv(hProcess);
-
-	if (!searchMonoModule(hv, monoModule)) return;
-
-	Injector inj(&hv, monoModule);
-
-	inj.eject(assembly, nmspaceName, className, methodName);
-}
-
 int main()
 {
 	if constexpr (sizeof(void*) != 8)
@@ -148,15 +78,6 @@ int main()
 	inj.eject(assm, "TestInject", "Loader", "Unload");
 
 	CloseHandle(hProcess);
-
-	MonoAssembly *const a = inject(
-		"C:\\Users\\endar\\Desktop\\TestInject\\TestInject\\bin\\Debug\\TestInject.dll",
-		"TestInject",
-		"Loader",
-		"Load"	
-	);
-
-	enject(a, "TestInject", "Loader", "Unload");
 
 	return 0;
 }
